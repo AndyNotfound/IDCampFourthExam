14435919ae043f9d58a79cb8b8256fb71c98a422
AndyNotfound <And.yNotfound@gmail.com>
2023-01-04 12:18:13 -0500

    Up: All done, probably, idk man.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,46 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.scss';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.scss";
 
-const hero = document.querySelector('#hero');
-const main = document.querySelector('main');
-const drawer = document.querySelector('#drawer');
-const menu = document.querySelector('#hamburgerMenu');
+const drawer = document.querySelector("#drawer");
+const menu = document.querySelector("#hamburgerMenu");
 
-menu.addEventListener('click', e => {
-  drawer.classList.toggle('open');
+menu.addEventListener("click", (e) => {
+  drawer.classList.toggle("open");
   e.stopPropagation();
+});
+
+function makeArticle(data) {
+  const article = document.createElement("article");
+  article.innerHTML = `
+      <!-- Article Card -->
+      <img src="${data.pictureId}" alt="${data.name} restaurant">
+      <div class="city"><p>${data.city}</p></div>
+      <div class="detail">
+        <p class="rating">Rating : ${data.rating}</p>
+        <a href="#detail" class="tittle">${data.name}</a>
+        <p class="desc">${data.description}</p>
+        </div>
+  `;
+  return article;
+}
+
+const data = require("../DATA.json");
+document.addEventListener("DOMContentLoaded", () => {
+  const { restaurants } = data;
+  const articleWrapper = document.querySelector("#article-container");
+  const recomendationWrapper = document.querySelector(
+    "#recommendation-container"
+  );
+
+  restaurants.map((item) => {
+    if (parseFloat(item.rating) >= 4.6) {
+      if (recomendationWrapper.children.length < 4) {
+        recomendationWrapper.append(makeArticle(item));
+      } else {
+        articleWrapper.append(makeArticle(item));
+      }
+    } else {
+      articleWrapper.append(makeArticle(item));
+    }
+  });
 });
